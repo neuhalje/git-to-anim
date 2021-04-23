8cf10c33bd16f54dcd0b2ecac0c4323a94d4a878
Jens Neuhalfen <jens@neuhalfen.name>
2021-04-23 07:27:06 +0200

    load from yaml

--- main.py
+++ main.py
@@ -1,16 +1,163 @@
-# This is a sample Python script.
+from typing import Type, Dict, List
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import yaml
+
+UserId = str
+RepoId = str
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+class User(object):
+
+    def __init__(self, user_id: UserId, name: str, email: str):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+
+
+    def validate(self, context):
+        assert self.email and len(self.email) > 0, "User: email not set"
+        assert self.name and len(self.name) > 0, "User: name not set"
+        assert self.user_id and len(self.user_id) > 0, "User: user_id not set"
+
+
+UserRepo = Type[Dict[UserId, User]]
+
+
+class Remote(object):
+    def __init__(self, name: str, repo: RepoId):
+        self.repo = repo
+        self.name = name
+
+    def validate(self, context):
+        assert self.repo and len(self.repo) > 0, "Remote: repo not set"
+        assert context.git_repos[self.repo], f"Remote: Context should contain " \
+                                             f"repo named ${self.repo}"
+
+        assert self.name and len(self.name) > 0, "Remote: name not set"
+
+
+Remotes = Type[Dict[str, Remote]]
+
+
+class GitRepo(object):
+    def __init__(self, repo_id: RepoId, path: str, bare: bool, user: UserId,
+                 remotes: Remotes = None):
+        self.repo_id = repo_id
+        self.remotes = remotes if remotes else {}
+        self.user = user
+        self.bare = bare
+        self.path = path
+
+    def validate(self, context):
+        assert self.repo_id and len(
+            self.repo_id) > 0, "GitRepo: repo_id not set"
+        assert self.user or self.bare, "GitRepo: user not set for none bare " \
+                                       "repo"
+        assert self.path, "GitRepo: path not set"
+
+        for r in self.remotes.values():
+            r.validate(context)
+
+
+GitRepos = Type[Dict[RepoId, GitRepo]]
+
+
+class ExecutionContext(object):
+    def __init__(self, user_repo: UserRepo, git_repos: GitRepos):
+        self.git_repos = git_repos
+        self.user_repo = user_repo
+
+    def validate(self):
+        for r in self.git_repos.values():
+            r.validate(self)
+
+        for u in self.user_repo.values():
+            u.validate(self)
+
+
+Command = str
+Commands = List[Command]
+
+
+class CommandSet(object):
+    def __init__(self, repo: RepoId, commands: Commands):
+        self.repo = repo
+        self.commands = commands
+
+    def validate(self, context: ExecutionContext):
+        assert self.repo in context.git_repos, f"CommandSet: Cannot find " \
+                                               f"repo ${self.repo} in  " \
+                                               f"context."
+
+
+CommandSets = List[CommandSet]
+
+
+class Script(object):
+    def __init__(self, script_id: str, context: ExecutionContext, commands: \
+            CommandSets):
+        self.id = script_id
+        self.context = context
+        self.commands = commands
+
+    def validate(self):
+        self.context.validate()
+        for c in self.commands:
+            c.validate(self.context)
+
+
+def load_script(script) -> Script:
+    s = yaml.safe_load(script)
+    script_id = s["id"]
+
+    user_repo: UserRepo = dict()
+    for user_id, user_dict in s["user"].items():
+        user_repo[user_id] = User(user_id, user_dict["name"],
+                                  user_dict["email"])
+
+    """
+    mine:
+      path: mine
+      bare: N
+      user: alice
+      remotes:
+        - name: origin
+          repo: upstream
+    """
+    git_repos: GitRepos = dict()
+    for repo_id, repo_dict in s["repos"].items():
+        remotes = dict()
+
+        for r in repo_dict.get("remotes", []):
+            remotes[str(r["name"])] = Remote(r["name"], r["repo"])
+
+        git_repos[repo_id] = GitRepo(repo_id,
+                                     repo_dict["path"],
+                                     repo_dict.get("bare", False),
+                                     repo_dict.get("user", None),
+                                     remotes)
+    current_repo = None
+    command_sets = list()
+
+    for cmd in s["commands"]:
+        script = cmd["script"]
+        # Allow "script: do something"
+        if isinstance(script, str):
+            script = [script]
+
+        current_repo = cmd.get("context") or current_repo
+
+        assert current_repo, "Context needs to be set"
+
+        command_sets.append(CommandSet(current_repo, script))
+
+    return Script(script_id, ExecutionContext(user_repo, git_repos),
+                  command_sets)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    s = load_script(open("tests/example-command.yaml", 'r'))
+    s.validate()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
