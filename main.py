877b08fdd5cf9a6e8df6cf7c28790cf535ef8077
Jens Neuhalfen <jens@neuhalfen.name>
2021-04-23 19:47:36 +0200

    now

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import tempfile
 from typing import Type, Dict, List
 
 import yaml
@@ -30,7 +32,7 @@ class Remote(object):
     def validate(self, context):
         assert self.repo and len(self.repo) > 0, "Remote: repo not set"
         assert context.git_repos[self.repo], f"Remote: Context should contain " \
-                                             f"repo named ${self.repo}"
+                                             f"repo named {self.repo}"
 
         assert self.name and len(self.name) > 0, "Remote: name not set"
 
@@ -74,13 +76,49 @@ class ExecutionContext(object):
             u.validate(self)
 
 
+class CommandExecutionResult(object):
+    def __init__(self, command, formatted_command:str, stdout: str, stderr:
+    str,
+                 exit_code: int, pwd: str):
+        self.formatted_command = formatted_command
+        self.pwd = pwd
+        self.stderr = stderr
+        self.stdout = stdout
+        self.command = command
+        self.exit_code = exit_code
+
+
 class Command(object):
     def __init__(self, cmd: str):
         self.cmd = cmd
 
-    def format(self, context: ExecutionContext, git_repo: GitRepo):
+    def format(self, context: ExecutionContext, git_repo: GitRepo) -> str:
         user = context.user_repo.get(git_repo.user)
         return self.cmd.format(repo=git_repo, user=user)
+
+    def _ensure_in_dir(self, directory: str):
+        if os.path.isfile(directory):
+            raise ValueError(f"{directory} is a file!")
+
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+        os.chdir(directory)
+
+    def execute(self, wdir: str, context: ExecutionContext,
+                git_repo: GitRepo) -> CommandExecutionResult:
+
+        cwd = os.getcwd()
+        # git_repo.path can be relative. Ensure we are in wdir
+        self._ensure_in_dir(wdir)
+        command = self.format(context, git_repo)
+
+        try:
+            self._ensure_in_dir(git_repo.path)
+            print(command)
+        finally:
+            os.chdir(cwd)
+        return CommandExecutionResult(self, command, "stdout", "stderr", 0,
+                                      git_repo.path)
 
 
 Commands = List[Command]
@@ -93,15 +131,25 @@ class CommandSet(object):
 
     def validate(self, context: ExecutionContext):
         assert self.repo in context.git_repos, f"CommandSet: Cannot find " \
-                                               f"repo ${self.repo} in  " \
+                                               f"repo {self.repo} in  " \
                                                f"context."
 
     def _format_commands(self, context: ExecutionContext):
         repo = context.git_repos[self.repo]
         return [c.format(context, repo) for c in self.commands]
 
+    def execute(self, wdir: str, context: ExecutionContext):
+        repo = context.git_repos[self.repo]
+        return [c.execute(wdir, context, repo) for c in self.commands]
+
 
 CommandSets = List[CommandSet]
+
+
+class ScriptResult(object):
+    def __init__(self, wdir: str, results: List[CommandExecutionResult]):
+        self.results = results
+        self.wdir = wdir
 
 
 class Script(object):
@@ -116,6 +164,15 @@ class Script(object):
         for c in self.commands:
             c.validate(self.context)
 
+    def execute(self) -> ScriptResult:
+        self.validate()
+        wdir = tempfile.mkdtemp()
+
+        results = []
+        for commandset in self.commands:
+            results.extend(commandset.execute(wdir, self.context))
+
+        return ScriptResult(wdir, results)
 
 def load_script(script) -> Script:
     s = yaml.safe_load(script)
@@ -171,6 +228,10 @@ if __name__ == '__main__':
     s = load_script(open("tests/example-command.yaml", 'r'))
     s.validate()
 
-    for cs in s.commands:
-        for c in cs._format_commands(s.context):
-            print(c)
+    result = s.execute()
+
+    print(f"temp: {result.wdir}")
+    for cr  in result.results:
+        print(cr.formatted_command)
+
+
